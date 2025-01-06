fb56767c05069226974d4d667fcfb6c64862c0e9
Fuzer <fuuzer_hotmail.com>
2025-01-06 20:56:46 -0300

    started js questions

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,7 @@
+const container = document.querySelector('main');
+const title = document.querySelector('.question-title');
+const options = document.querySelector('.options');
+
 class Question {
   constructor(question, options, correctAnswer) {
     this.question = question;
@@ -15,3 +19,22 @@ const quizQuestions = [
    new Question('Por que a maçã é vermelha?', ['Jonas broter', 'ohayoo', 'asuidfhsdfh', 'maçã'], 3)
 ]
 
+// quizQuestions.forEach((question, index) => {
+//   console.log(`Pergunta ${index + 1}: ${question.question}`);
+//   console.log('Opções:');
+//   question.options.forEach((option, i) => {
+//     console.log(`${i + 1}. ${option}`)
+//   })
+// });
+
+quizQuestions.forEach((question, index) => {
+container.innerHTML = `
+  <div class="question">
+    <!-- <p class="question-number">Pergunta 1</p> -->
+    <h3 class="question-title">${question.question}</h2>
+  </div>
+
+  <ul class="options">
+    ${question.options.map((option, i) => `<li class="option">${option}</li>`).join('')}
+  </ul>`;
+});
